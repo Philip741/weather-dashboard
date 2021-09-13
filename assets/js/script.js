209d3c0437539911a6d5b03a405a27f7f8b88dec
Philip Browning <philip741@gmail.com>
2021-09-13 13:55:51 -0400

    Added function to populate 5 day forecast Modified html to set 1 as the starting index for elements that are dynamically generated like temp, wind, humidity

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,12 @@ var now = dayjs();
 
 var searchButton = document.getElementById("search-button");
 var appId = "b3f1aa24ac8904093849c6e0bd81b58f";
-
+var cityId = 0;
+const imageUrl = 'https://openweathermap.org';
 
 console.log(now.format('MM/DD/YYYY'));
 searchButton.addEventListener('click', function () {
+    cityId++;
     const city = document.getElementById("city-search").value;
     const currentWeather = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${appId}`
     const forecast = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${appId}`
@@ -15,7 +17,7 @@ searchButton.addEventListener('click', function () {
     console.log(city);
     cityNameEl.textContent = city.toUpperCase() + "(" + todayDate + ")";
     getCurrWeather(currentWeather);
-    getFiveDay(forecast);
+    saveCity(cityId,city);
 } )
 
 async function getCurrWeather (url) {
@@ -29,16 +31,35 @@ async function getCurrWeather (url) {
     let wind = data.wind.speed;
     let lat = data.coord.lat;
     let lon = data.coord.lon;
+    let icon = data.weather[0].icon;
+    let forecastUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${appId}`
 
     getUvIndex(lat,lon,appId);
-    populateWeather(temp,wind,humidity,);
+    displayIcons(imageUrl,icon,"current-icon","large");   
+    populateWeather(temp,wind,humidity);
+    getFiveDay(forecastUrl);
 }
 
 async function getFiveDay (url) {
     const response = await fetch(url);
 
     let data = await response.json();
-    console.log(data.list[0]);
+    //console.log(data.list[0]);
+    // starts at 0 for html elements and array
+    for (let i=1; i < 6; i++) {
+        console.log(data.daily[i]);
+        let tempEl = document.getElementById(`temp${i}`);
+        let windEl = document.getElementById(`wind${i}`);
+        let humidityEl = document.getElementById(`humidity${i}`);
+        let dateEl = document.getElementById(`day${i}`);
+        let timeStamp = data.daily[i].dt;
+        let icon = data.daily[i].weather[0].icon;
+        dateEl.textContent = dayjs.unix(timeStamp).format('ddd');
+        displayIcons(imageUrl,icon,`icon${i}`,"large");   
+        tempEl.textContent = "Temp: " + data.daily[i].temp.day; 
+        windEl.textContent = `Wind: ${data.daily[i].wind_speed}`; 
+        humidityEl.textContent = `Humidity: ${data.daily[i].humidity}`; 
+    }
 }
 
 async function getUvIndex(lat,lon,appId) {
@@ -47,6 +68,7 @@ async function getUvIndex(lat,lon,appId) {
     const response = await fetch(uvIndexUrl);
 
     let data = await response.json();
+    console.log(data);
     uvIndex = parseInt(data.current.uvi);
     uvIndexEl.textContent = "UV index: " + uvIndex;
     if (uvIndex <= 2){
@@ -77,5 +99,30 @@ function populateWeather(temp,wind,humidity) {
 }
 
 function populateFiveDay (url) {
+ console.log(url)
+}
 
+function displayHide () {
+    
+} 
+
+function saveCity (cityId,cityName) {
+    // save city name to local storage and generate button 
+    localStorage.setItem(cityId,cityName);
+    console.log(localStorage);
+}
+
+function clearCities () {
+    // clear cities from local storage
+}
+
+function displayIcons(url, iconId, icon, size) {
+  let popIcon = document.getElementById(`${icon}`);
+  //console.log(popIcon);
+  if (size === "small") {
+  popIcon.src = url + `/img/wn/${iconId}.png`
+  }
+  else if (size === "large") {
+  popIcon.src = url + `/img/wn/${iconId}@2x.png`
+  }
 }
